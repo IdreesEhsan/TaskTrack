daa1e6ed778c81afcd093a96650c8918f4646ee5
IdreesEhsan <idreesehsan479@gmail.com>
2026-07-27 11:01:37 +0500

    Added the task functionality for specified users

---
+++ backend/models/Task.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema(
+    {
+        // Links the task to the User who created it.
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+            default: '',
+        },
+        status: {
+            type: String,
+            enum: ['Pending', 'in-progress', 'completed'], // Only these 3 values allowed
+            default: 'pending',
+        },
+        dueDate: {
+            type: Date,
+        },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model('Task', taskSchema);
